53bed323b314e2c98dfb838fd72d9416fd845659
Hariom Singh <21bcs5872@cuchd.in>
2025-01-03 09:47:16 +0530

    add filter functionality

--- frontend/src/components/filter/Filter.jsx
+++ frontend/src/components/filter/Filter.jsx
@@ -3,7 +3,46 @@ import "./filter.scss";
 const Filter = () => {
     return (
         <div className="filter">
-            
+            <h1>Search results for <b>Chandigarh</b></h1>
+            <div className="top">
+                <div className="item">
+                    <label htmlFor="city">Location</label>
+                    <input type="text"  id="city" name="city" placeholder="City Location"/>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="item">
+                    <label htmlFor="type">Type</label>
+                    <select name="type" id="type">
+                        <option value="buy">Buy</option>
+                        <option value="rent">Rent</option>
+                    </select>
+                </div>
+                <div className="item">
+                    <label htmlFor="property">Property</label>
+                    <select name="property" id="property">
+                        <option value="apartment">Apartment</option>
+                        <option value="house">House</option>
+                        <option value="condo">Condo</option>
+                        <option value="land">Land</option>                        
+                    </select>
+                </div>
+                <div className="item">
+                    <label htmlFor="city"></label>
+                    <input type="text" />
+                </div>
+                <div className="item">
+                    <label htmlFor="city"></label>
+                    <input type="text" />
+                </div>
+                <div className="item">
+                    <label htmlFor="city"></label>
+                    <input type="text" />
+                </div>
+                <button>
+                    <img src="/search.png" alt="" />
+                </button>
+            </div>
         </div>
     )
 }
